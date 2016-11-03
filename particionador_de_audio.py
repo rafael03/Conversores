093096a658e6e9db26090920203febd70d5b2cd1
Rafael P <rafaelrps11@gmail.com>
2016-11-02 22:42:44 -0200

    Update particionador_de_audio.py
    Limpeza do código para remover geração de página html

--- particionador_de_audio.py
+++ particionador_de_audio.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 import os
 from os import walk
+import sys
+# print sys.argv[1]
 
 lista_de_arquivos = []
 for (dirpath, dirnames, filenames) in walk(os.getcwd()):
@@ -10,7 +12,7 @@ for (dirpath, dirnames, filenames) in walk(os.getcwd()):
 
 for arquivo in lista_de_arquivos:
 	if arquivo[-4:] == ".mp3":
-		os.system("mp3splt %s -S 5" % arquivo)
+		os.system("mp3splt %s -S 7" % arquivo)
 		os.system("rm %s" % arquivo)
 
 lista_particionada = []
@@ -23,32 +25,4 @@ lista_particionada.sort()
 print "lista normal", lista_particionada
 for arquivo in range(0, len(lista_particionada)):
 	if lista_particionada[arquivo][-4:] == ".mp3":
-		# os.system("mv %s" % arquivo)
 		os.system("mv %s %s" % (lista_particionada[arquivo], str(arquivo)))
-
-
-
-# for arquivo in lista_de_arquivos:
-# 	if arquivo[-4:] == ".mp3":
-
-
-# play = open('play.html', 'w+')
-# play.write('<!DOCTYPE html>')
-# play.write('<html>')
-# play.write('<head>')
-# play.write('<meta charset="UTF-8">')
-# play.write('</head>')
-# play.write('<body>')
-# for arquivo in lista_de_arquivos:
-# 	if arquivo[-4:] == ".mp4":
-# 		play.write('<h1>Aula: %s </h1>' %str(arquivo))
-# 		play.write('<video width="480" height="300" controls>')
-# 		play.write('<source src="%s" type="video/mp4">' % str(arquivo))
-# 		play.write('Seu navegador não suporta o elemento <code>video</code>.')
-# 		play.write('</video>')
-# 		play.write('</br>')
-# play.write('</body')
-# play.write('</html>')
-# play.close()
-
-# print "Página HTML gerada com sucesso!"
